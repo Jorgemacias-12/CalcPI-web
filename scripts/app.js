cbf947bbf10d0606a78629bcbe1a75d00b9b72cf
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-03-09 00:45:49 -0600

    Definiendo evento donde se carga la página y métodos que ayudan a la carga de algunos elementos.

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    document.getElementById('ma-1').addEventListener("click",  () => {MA();});
+    document.getElementById('mm-2').addEventListener("click", () => {MM();});
+    document.getElementById('mb-3'.addEventListener("click"), () => {MB();});
+}
+
+const MA = () => {
+    
+}
+
+const MM = () => {
+
+}
+
+const MB = () => {
+
+}
