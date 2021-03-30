7adc59c30551f3d1c96ce53410e824e770446f73
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-03-29 18:08:35 -0600

    Se han refactorizado las funciones

---
+++ scripts/methods.js
@@ -0,0 +1,57 @@
+let calculateMA = (number) => {
+    const r = 1;
+    let a;
+    let b;
+    a = 4 * Math.sqrt(2) * r;
+    b = 8 * r;
+    let m = 4;
+    while (m * 2 <= number) {
+        b = 2 * a * b / (a + b);
+        a = Math.sqrt(a * b);
+        m = m * 2;
+    }
+    let pi = (a / 2 / r + b / 2 / r) / 2;
+    let err = Math.abs(a / 2 / r - b / 2 / r) / 2;
+    return new ResultTemplate(m, pi, err);
+}
+
+const calculateMM = (number) => {
+    const r = 1;
+    let x;
+    let y;
+    let cota = 10;
+    let c = 0;
+    let pi_arr = new Array(cota);
+    for (let i = 0; i < cota; i++) {
+        for (let j = 0; j < number; j++) {
+            x = Math.random();
+            y = Math.random();
+            x = x * r;
+            y = y * r;
+            if (x * x + y * y < r * r) {
+                c++
+            }
+        }
+        pi_arr[i] = 4 * c / number;
+        c = 0;
+    }
+    let pi = 0;
+    let err = 0;
+    for (let i = 0; i < cota; i++) {
+        pi = pi_arr[i] / cota + pi;
+    }
+    for (let i = 0; i < cota; i++) {
+        err = err + Math.pow(pi - pi_arr[i], 2) / cota;
+    }
+    err = Math.sqrt(err);
+    return new ResultTemplate(null, pi, err);
+}
+
+const calculateMB = (number) => {
+    let s = 0;
+    for (let i = 0; i < number; i++) {
+        s = s + 1 / Math.pow(i + 1, 2);
+    }
+    let pi = Math.sqrt(6 * s);
+    return new ResultTemplate(null, pi, null);
+}
